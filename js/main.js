cb1b5e0f3fe11c5f1586c6f0ba089b24545d9dbc
tnwatts <tnwatts@live.com>
2022-07-01 17:37:57 -0600

    sphere gradients for pegs

--- js/main.js
+++ js/main.js
@@ -1,5 +1,29 @@
 /*----- constants -----*/
+// const pegs ={
+// red: {
+    
+//     value:   'radial-gradient(circle at 65% 15%, white 1px, red 3%, brown 60%, red 100%)',
+// }
+ //   green: 'radial-gradient(circle at 65% 15%, white 1px, aqua 3%, darkblue 60%, aqua 100%)',
+// blue:  'radial-gradient(circle at 65% 15%, white 1px, aqua 3%, darkblue 60%, aqua 100%)',
+// yellow:'radial-gradient(circle at 65% 15%, white 1px, aqua 3%, darkblue 60%, aqua 100%)',
+// brown:'radial-gradient(circle at 65% 15%, white 1px, aqua 3%, darkblue 60%, aqua 100%)',
+// orange:'radial-gradient(circle at 65% 15%, white 1px, aqua 3%, darkblue 60%, aqua 100%)',
+// black:'radial-gradient(circle at 65% 15%, white 1px, aqua 3%, darkblue 60%, aqua 100%)',
+// white:'radial-gradient(circle at 65% 15%, white 1px, aqua 3%, darkblue 60%, aqua 100%)'
+// }
 /*----- app's state (variables) -----*/
 /*----- cached element references -----*/
+const selectorEls = document.querySelectorAll('.selector');
 /*----- event listeners -----*/
 /*----- functions -----*/
+
+function init() {
+    // selectorEls.forEach(function(selecorEl, idx){
+
+    // })
+        // selectorEl.backgroundColor = pegs[idx]
+    // selectorEls[0].style.background = pegs[1];
+}
+
+init();
