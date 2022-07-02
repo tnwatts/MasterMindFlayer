455cc6e618b407ae246e9794858ddc16602822a9
tnwatts <tnwatts@live.com>
2022-07-02 03:11:21 -0600

    Got the layoutPegs function working, added to init,continued improvements on styling

--- js/main.js
+++ js/main.js
@@ -14,11 +14,13 @@
 /*----- app's state (variables) -----*/
 let gameRoundsLeft; //10 rounds means 10 guesses
 let gameStatus; //game in session = null, victory = 'W', loser = 'L'
-let guessSlot; //array to hold the current 4 guess's
+let guessSlot; //array to hold the current play group
+let currentGuess; //array that holds the current guesses
 let guessCount; //int to keep track of how many guess there are
 /*----- cached element references -----*/
 const selectorEls = document.querySelectorAll('.selector');
-const guessEls = document.querySelectorAll('#guess');
+const guessEls = document.querySelectorAll('.peg-cell-guess');
+const clueEls = document.querySelectorAll('.peg-cell-clue');
 /*----- event listeners -----*/
 document.querySelector('side').addEventListener('click', handlePegSelector);
 /*----- functions -----*/
@@ -27,8 +29,10 @@ init();
 function init() {
     gameStatus = null;
     gameRoundsLeft = 9;
-    guessCount = 0;
+
+    currentGuess = [];
     loadGuessEls();
+    layoutDivs();
     // selectorEls.forEach(function(selecorEl, idx){
 
     // })
@@ -38,24 +42,40 @@ function init() {
 }
 
 function render() {
-
+    //assign current guesses to elements
+    //guessSlot[currentGuess.length-1].setAttribute('id', `${currentGuess}`);
 }
 
-
 function handlePegSelector(evt){
+    //update the current guesses array with the peg selected
     if (evt.target.classList.value === 'selector'){
-        if (guessCount > 3) {
+        if (currentGuess.length >= 4) {
             alert('guesses full');
             return;
         }
-        guessSlot[guessCount++].setAttribute('id', `${evt.target.id}`);
+        currentGuess.push(evt.target.id);
         
         console.log(evt.target.id);
     }
+}
 
-    
+function handlePegRemover(){
+    //check if guessSlot array is empty, if true send message
+    //remove a peg from the guessSlot array
 }
 
 function loadGuessEls(){
     guessSlot = guessEls[gameRoundsLeft].children;
 }
+function layoutDivs(){
+    let allEls = [...guessEls, ...clueEls];
+    allEls.forEach(function(El){
+        for(i = 0; i <4; i++) {
+            let peg = document.createElement('peg');
+            peg.setAttribute('class','empty');
+            El.appendChild(peg);
+        }
+    })
+    console.log(allEls);
+}
+
