332c6836499a02437804445172433c9b324935da
tnwatts <tnwatts@live.com>
2022-07-01 20:25:20 -0600

    got some java script working and adding more div ID's

--- js/main.js
+++ js/main.js
@@ -1,29 +1,53 @@
 /*----- constants -----*/
-// const pegs ={
-// red: {
-    
-//     value:   'radial-gradient(circle at 65% 15%, white 1px, red 3%, brown 60%, red 100%)',
-// }
- //   green: 'radial-gradient(circle at 65% 15%, white 1px, aqua 3%, darkblue 60%, aqua 100%)',
-// blue:  'radial-gradient(circle at 65% 15%, white 1px, aqua 3%, darkblue 60%, aqua 100%)',
-// yellow:'radial-gradient(circle at 65% 15%, white 1px, aqua 3%, darkblue 60%, aqua 100%)',
-// brown:'radial-gradient(circle at 65% 15%, white 1px, aqua 3%, darkblue 60%, aqua 100%)',
-// orange:'radial-gradient(circle at 65% 15%, white 1px, aqua 3%, darkblue 60%, aqua 100%)',
-// black:'radial-gradient(circle at 65% 15%, white 1px, aqua 3%, darkblue 60%, aqua 100%)',
-// white:'radial-gradient(circle at 65% 15%, white 1px, aqua 3%, darkblue 60%, aqua 100%)'
-// }
+ const pegs = [
+    'empty',
+    'red',
+    'green',
+    'blue',
+    'yellow',
+    'brown',
+    'orange',
+    'black',
+    'white'
+];
+
 /*----- app's state (variables) -----*/
+let gameRound; //10 rounds means 10 guesses
+let gameStatus; //game in session = null, victory = 'W', loser = 'L'
+let guessSlot; //array to hold the current 4 guess's
 /*----- cached element references -----*/
 const selectorEls = document.querySelectorAll('.selector');
+const guessEls = document.querySelectorAll('#guess');
 /*----- event listeners -----*/
+document.querySelector('side').addEventListener('click', handlePegSelector);
 /*----- functions -----*/
+init();
 
 function init() {
+    gameStatus = null;
+    gameRound = 0;
+    loadGuessEls();
     // selectorEls.forEach(function(selecorEl, idx){
 
     // })
         // selectorEl.backgroundColor = pegs[idx]
     // selectorEls[0].style.background = pegs[1];
+    render();
 }
 
-init();
+function render() {
+
+}
+
+
+function handlePegSelector(evt){
+    if (evt.target.classList.value === 'selector'){
+        console.log(evt.target.classList);
+    }
+
+    gameRound++;
+}
+
+function loadGuessEls(){
+
+}
